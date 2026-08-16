2ae83e91d4760b4637fb4eabef022c8f93a65fba
verakissyou17 <130534541+verakissyou17@users.noreply.github.com>
2026-08-16 13:42:44 +0300

    Init repo

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [information, setInformation] = useState([]);
+  const [hovered, setHovered] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("./data.json");
+
+        if (!response.ok) {
+          throw new Error("Couldn't fetch the data");
+        }
+
+        const data = await response.json();
+        setInformation(data);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <main className="container">
+        <div className="header">
+          <section className="header-section">
+            <h1>My balance</h1>
+            <p className="total">$921.48</p>
+          </section>
+          <div className="img-container">
+            <img src="./logo.svg" alt="logo" />
+          </div>
+        </div>
+        <div className="main">
+          <h2>Spending - Last 7 days</h2>
+          <div className="bars-container">
+            {information.map((info, index) => {
+              return (
+                <div key={index} className="bar-box">
+                  <div
+                    className="tooltip"
+                    style={{ visibility: hovered === index ? "visible" : "hidden" }}
+                  >
+                    ${info.amount}
+                  </div>
+                  <span
+                    className="bar"
+                    style={{
+                      height: `${info.amount * 2}px`,
+                      filter: hovered === index ? "brightness(125%)" : "brightness(100%)",
+                    }}
+                    onMouseEnter={() => setHovered(index)}
+                    onMouseLeave={() => setHovered(null)}
+                  ></span>
+                  <p className="day">{info.day}</p>
+                </div>
+              );
+            })}
+          </div>
+          <div className="footer">
+            <div>
+              <h3>Total this month</h3>
+              <span className="monthly-total">$478.33</span>
+            </div>
+            <div className="footer-percent">
+              <span>+2.4%</span>
+              <p>from last month</p>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+        Coded by <a href="https://www.frontendmentor.io/profile/verakissyou17">Vera</a>.
+      </footer>
+    </>
+  );
+}
+
+export default App;
